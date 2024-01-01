ee0236a064f320189278d443dcaefa2ce097c774
Cleo <cprubino25@gmail.com>
2024-01-01 15:02:32 +0800

    Add styling and options when sketching

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
-var button = document.querySelector("#prompt")
-var activeDiv = document.querySelectorAll("div")
-button.addEventListener('click', () => {
-    document.body.innerHTML = ""
+var gridButton = document.querySelector("#prompt")
+var drawingSection = document.getElementsByClassName("drawingSection")
+var penColor = 'black'
+
+document.getElementById("black").addEventListener("click", () => {
+    penColor = "black"
+})
+
+document.getElementById("random").addEventListener("click", () => {
+    penColor = "random"
+})
+
+document.getElementById("eraser").addEventListener("click", () => {
+    penColor = "whitesmoke"
+})
+
+gridButton.addEventListener('click', () => {
+    drawingSection[0].innerHTML="";
+    var input = document.getElementById("input")
     var container = document.createElement("div")
     container.classList.add("container")
-    container.style.cssText = `display:grid; background-color:whitesmoke; width:100%; height:100%;`
-    document.body.append(container)
-    let input = parseInt(prompt("Insert Grid")).toFixed(0)
-    if (isNaN(input)|| input == undefined){
-        alert("Input is not an integer")
-    } else {
-        for(var x = 0; x<input; x++) {
-            addContainer(input, container)
-            
+    container.style.cssText = `display:grid; width:100%; height:100%;`
+    drawingSection[0].appendChild(container)
+    let parsedInput = parseInt(input.value).toFixed(0)
+    if (isNaN(parsedInput)|| parsedInput == undefined || parsedInput > 100){
+        alert("Invalid input - not number or greater than 100")
+        window.location.reload()
+    } 
+    else {
+        for(var x = 0; x<parsedInput; x++) {
+            addContainer(parsedInput, container)
+            var activeDiv = document.querySelectorAll(".box")
+            activeDiv.forEach((d) => {
+                d.addEventListener("mouseover", ()=>{
+                    d.style.backgroundColor = penColor === "random" ? '#' + Math.random().toString(16).substring(9) : penColor
+                })
+            })
          }
     }
 })
 
-
 function addContainer(grid = 1, element) {
     var row = document.createElement("div")
     row.classList.add("container")
-    row.style.cssText = `display:flex; background-color:whitesmoke; width:100%; height:100%;`
+    row.style.cssText = `display:flex;width:100%; height:100%;`
     element.appendChild(row)
 
     for(var x = 0; x<grid; x++) {
         var div = document.createElement("div")
-        div.style.width = "100%";
+        div.classList.add("box")
+        div.style.cssText = 'width:100%; border:thin solid gray;'
         row.appendChild(div)
     }
 }
